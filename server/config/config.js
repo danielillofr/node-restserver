edffee1e516b1f0fde5714ccd2fd2abe561d1cdf
Dani Fdez <dfdezruiz2@gmail.com>
2018-09-26 23:32:29 +0200

    Cambiando variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb://danielillofr:mlabdan123ABC@ds115613.mlab.com:15613/cafe';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://danielillofr:mlabdan123ABC@ds115613.mlab.com:15613/cafe';
+}
 
 process.env.URLDB = urlDB;
